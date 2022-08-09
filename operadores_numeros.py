2aba424a96dd37d301b484d640e1346e9240e42b
joaoGabArruda <107219050+joaoGabArruda@users.noreply.github.com>
2022-08-09 08:28:51 -0400

    Create operadores_numeros.py

---
+++ operadores_numeros.py
@@ -0,0 +1,14 @@
+# esses são todos os operadores que podem ser usados para números
+
+print(5+2) # --> 7
+print(5-2) # --> 3
+print(5/2) # --> 2.5 (a divisão usando barra única sempre retorna "float"
+print(5//2) # --> mostra o piso da divisão --> 2
+print(5%2) # pega o resto da divisão --> 1
+print(5*2) # --> 10
+print(5**2) #exponenciação --> 25
+print(-3**2) # --> -9
+print((-3)**2) # --> 9
+
+# operadores com operandos de diferentes tipos convertem o inteiro para ponto flutuante:
+print(4 * 3.75 - 1) # --> 14.0
